825dd9d5a9e19332a63003e52ac2d6be9741c6fe
ShurikElabuga <ShurikElabuga@yandex.ru>
2025-05-11 22:14:26 +0300

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -1,18 +1,19 @@
 package ru.job4j.dreamjob.repository;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Candidate;
-import ru.job4j.dreamjob.model.Vacancy;
-
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
+@ThreadSafe
 @Repository
 public class MemoryCandidateRepository implements CandidateRepository {
-private int nextId = 1;
-private final Map<Integer, Candidate> candidates = new HashMap<>();
+private final AtomicInteger nextId = new AtomicInteger(1);
+private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
 private MemoryCandidateRepository() {
     save(new Candidate(0, "Candidate for Intern Java Developer", "Description of the Candidate for intern"));
@@ -25,7 +26,7 @@ private MemoryCandidateRepository() {
 
     @Override
     public Candidate save(Candidate candidate) {
-        candidate.setId(nextId++);
+        candidate.setId(nextId.getAndIncrement());
         candidates.put(candidate.getId(), candidate);
         return candidate;
     }
